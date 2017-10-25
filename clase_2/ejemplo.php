6c77569d4af950375129993b887f21a31240033d
Matias <matias@matias.com>
2017-10-25 20:16:19 -0300

    se agrega ejemplo con switch

--- clase_2/ejemplo.php
+++ clase_2/ejemplo.php
@@ -1,7 +1,7 @@
 <?php
 
-$numero_1 = 8;
-$numero_2 = "8";
+$numero_1 = 0;
+$numero_2 = "0";
 
 if ($numero_1 == $numero_2){
 	print "Son iguales </br>";
@@ -12,4 +12,26 @@ if ($numero_1 === $numero_2){
 	print "Son iguales </br>";
 } else{
 	print "No son iguales </br>";
+}
+
+
+switch ($numero_1) {
+	case '0':
+		echo "La variable es 0";
+		break;
+	case '1':
+		echo "La variable es 1";
+		break;
+	case '2':
+		echo "La variable es 4";
+		break;
+	case '3':
+		echo "La variable es 3";
+		break;
+	case '4':
+		echo "La variable es 4";
+		break;
+	default:
+		echo "La variable no es ni 0, 1, 2, 3 ni 4.";
+		break;
 }
