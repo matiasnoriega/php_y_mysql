9eff534d29e5cf584a743e4c9631621227934c7d
Matias Noriega <matiasnoriega86@gmail.com>
2017-10-27 21:06:29 -0300

    se termina laboratorio de clase 2

---
+++ clase_2/tabla_dinamica.php
@@ -0,0 +1,61 @@
+<?php
+
+$filas = 10;
+$columnas = 15;
+
+$tabla = "<table>";
+
+for($i = 0; $i < $filas; $i++){
+
+	$tabla .= "<tr>";
+
+	for($j = 0; $j < $columnas ; $j++){
+
+		if(($i % 2) == 0){
+		
+			$tabla .= "<td style='border: 1px solid; height: 30px; width: 30px; background-color: black'></td>";
+		
+		}else{
+
+			$tabla .= "<td style='border: 1px solid; height: 30px; width: 30px; background-color: orange'></td>";
+
+		}
+	}
+
+	$tabla .= "</tr>";
+
+}
+
+$tabla .= "</table>";
+
+echo $tabla;
+
+$tabla2 = "<table>";
+
+$i = 0;
+
+while($i < $filas){
+
+	$tabla2 .= "<tr>";
+	$j = 0;
+
+	while ($j < $columnas) {
+
+		if(($j % 2) == 0){
+
+			$tabla2 .= "<td style='border: 1px solid; height: 30px; width: 30px; background-color: pink'></td>";
+
+		}else{
+			
+			$tabla2 .= "<td style='border: 1px solid; height: 30px; width: 30px; background-color: green'></td>";
+		}	
+		$j++;
+
+	}
+
+	$i++;
+}
+
+$tabla2 .= "</table>";
+
+echo $tabla2;
